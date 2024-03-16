35940ab789799870be021d205a95363874a5550e
Tali Auster <taliauster@gmail.com>
2024-03-16 16:48:07 -0600

    baumgartner: enable postgres auth remotely

--- baumgartner/services/postgres.nix
+++ baumgartner/services/postgres.nix
@@ -3,11 +3,12 @@
 {
   services.postgresql = {
     enable = true;
-
     dataDir = "/data/postgres/15";
 
+    enableTCPIP = true;
     authentication = ''
       local	all	all	trust
+      host	all	all	100.64.0.0/10	scram-sha-256
     '';
   };
 }
