f3e947c7c7973f3a7ef3909a42f77a1417bb6668
Tali Auster <taliauster@gmail.com>
2024-03-26 12:36:15 -0600

    baumgartner: fix home-manager version
    Use 23.11 for 23.11 and unstable for unstable.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
   inputs.unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
-  inputs.home-manager.url = "github:nix-community/home-manager";
+  inputs.home-manager.url = "github:nix-community/home-manager/release-23.11";
   inputs.home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
   inputs.home-manager-unstable.url = "github:nix-community/home-manager";
