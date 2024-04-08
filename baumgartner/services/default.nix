6bac59bc5d65d15b3aa2e86d0308b423f54030a1
Tali Auster <taliauster@gmail.com>
2024-04-08 10:56:44 -0600

    do more distcc stuff
    idk what's going on lmao

--- baumgartner/services/default.nix
+++ baumgartner/services/default.nix
@@ -46,6 +46,12 @@
       stub = "in";
       port = 9090;
     }
+
+    # distcc
+    {
+      stub = "cc";
+      port = 3633;
+    }
   ];
 
   imports = [
@@ -53,4 +59,9 @@
   ];
 
   services.distccd.enable = true;
+  services.distccd.allowedClients = [ "127.0.0.1" "100.64.0.0/10" "192.168.0.0/16" ];
+  services.distccd.openFirewall = true;
+  services.distccd.stats.enable = true;
+  services.distccd.logLevel = "info";
+  environment.systemPackages = with pkgs; [ gcc clang ];
 }
