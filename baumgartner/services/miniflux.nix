48c8c016d2168313ac73b49cae3e80c06a1647ec
Tali Auster <me@tali.network>
2025-03-27 18:12:48 -0600

    baumgartner: configure OIDC for miniflux

--- baumgartner/services/miniflux.nix
+++ baumgartner/services/miniflux.nix
@@ -7,5 +7,11 @@
   services.miniflux.adminCredentialsFile = "/data/secrets/miniflux.env";
   services.miniflux.config = {
     LISTEN_ADDR = "0.0.0.0:1819";
+
+    OAUTH2_PROVIDER = "oidc";
+    OAUTH2_CLIENT_ID = "6011ce6b-c1b6-4aa2-b9df-cc120ef3eb18";
+    OAUTH2_REDIRECT_URL = "https://rss.tali.network/oauth2/oidc/callback";
+    OAUTH2_OIDC_DISCOVERY_ENDPOINT = "https://auth.tali.network";
+    OAUTH2_USER_CREATION = "1";
   };
 }
