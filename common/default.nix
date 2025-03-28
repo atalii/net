3955b4a209a458a8e431e7da833cf8e4ce7b39fc
Tali Auster <me@tali.network>
2025-03-28 19:37:55 +0000

    common: remove atalii

--- common/default.nix
+++ common/default.nix
@@ -11,17 +11,5 @@
 
   nixpkgs.config.allowUnfree = true;
 
-  programs.zsh.enable = true;
-  users.defaultUserShell = pkgs.zsh;
-
-  users.users.atalii = {
-    isNormalUser = true;
-    extraGroups = [ "networkmanager" "wheel" ];
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFYfx4uUXRxcWXGemF6zfwVIvqXOWKVchz78rWFJiwTk"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICUjegPPEjqyuN8pXNsu+rdSSVCtxOOEJEd6Abz8QMUp"
-    ];
-  };
-
   networking.firewall.allowedTCPPorts = [ 80 443 22 ];
 }
