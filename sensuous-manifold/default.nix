7fee856af73624a82ef32af1e68d7657b262cc67
Tali Auster <tali@sensuous-manifold.>
2025-04-03 23:59:44 +0000

    vps's: move jf from baum to sm

--- sensuous-manifold/default.nix
+++ sensuous-manifold/default.nix
@@ -1,11 +1,30 @@
-{ ... }: {
+{ ... }:
+
+let
+  baumgartner = "100.64.0.1";
+  proxy = host: port: auth: (if auth then ''
+    forward_auth localhost:9091 {
+      uri /api/authz/forward-auth
+      copy_headers Remote-User Remote-Groups Remote-Email Remote-Name
+    }
+  '' else "") + ''
+    reverse_proxy * "http://${host}:${toString port}"
+  '';
+
+  proxyBaum = proxy baumgartner;
+in {
   imports = [
     ./hardware-configuration.nix
     ./networking.nix # generated at runtime by nixos-infect
     
   ];
 
-    nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  services.caddy = {
+    enable = true;
+    virtualHosts."jellyfin.tali.network".extraConfig = proxyBaum 8096 false;
+  };
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   boot.tmp.cleanOnBoot = true;
   zramSwap.enable = false;
