0064e1fec59bb8021768b13a109086281f94ce94
Tali Auster <me@tali.network>
2025-04-04 16:11:32 +0000

    vps's: move most services to vancouver
    Drastically improve latency. I'll take care of fedi soon.

--- gardiner/configuration.nix
+++ gardiner/configuration.nix
@@ -37,21 +37,6 @@ in {
           flush_interval -1
       }
     '';
-
-    virtualHosts."code.tali.network".extraConfig = proxyBaum 4444 true;
-    virtualHosts."rss.tali.network".extraConfig = proxyBaum 1819 false;
-    virtualHosts."cal.tali.network".extraConfig = proxyBaum 8192 false;
-    virtualHosts."cabinet.tali.network".extraConfig = proxyBaum 6445 true;
-
-    virtualHosts."ttds.tali.network".extraConfig = proxy "100.90.198.6" 8080 false;
-
-    virtualHosts."wiki.tali.network".extraConfig = ''
-      rewrite /favicon.ico /static/art/favicons/tc-logo-green.ico
-      rewrite /_assets/favicons/android-chrome-192x192.png /static/art/favicons/tc-logo-green-192x192.png
-      rewrite /_assets/favicons/favicon-16x16.png /static/art/favicons/tc-logo-green-48x48.png
-
-      reverse_proxy * http://${baumgartner}:3000
-    '';
   };
 
   boot.tmp.cleanOnBoot = true;
--- sensuous-manifold/default.nix
+++ sensuous-manifold/default.nix
@@ -27,7 +27,21 @@ in {
     '';
 
     virtualHosts."auth.tali.network".extraConfig = proxy "localhost" 9091 false;
+    virtualHosts."cabinet.tali.network".extraConfig = proxyBaum 6445 true;
+    virtualHosts."cal.tali.network".extraConfig = proxyBaum 8192 false;
+    virtualHosts."code.tali.network".extraConfig = proxyBaum 4444 true;
     virtualHosts."jellyfin.tali.network".extraConfig = proxyBaum 8096 false;
+    virtualHosts."rss.tali.network".extraConfig = proxyBaum 1819 false;
+
+    virtualHosts."ttds.tali.network".extraConfig = proxy "100.90.198.6" 8080 false;
+
+    virtualHosts."wiki.tali.network".extraConfig = ''
+      rewrite /favicon.ico /static/art/favicons/tc-logo-green.ico
+      rewrite /_assets/favicons/android-chrome-192x192.png /static/art/favicons/tc-logo-green-192x192.png
+      rewrite /_assets/favicons/favicon-16x16.png /static/art/favicons/tc-logo-green-48x48.png
+
+      reverse_proxy * http://${baumgartner}:3000
+    '';
   };
 
   services.authelia.instances."tla" = {
