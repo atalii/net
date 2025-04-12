5cef795ad0cb082f8955df45d60d560594ee3f4a
Tali Auster <me@tali.network>
2025-04-12 13:04:31 -0600

    baum: put lean/elan in code-server's env

--- baumgartner/services/default.nix
+++ baumgartner/services/default.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
   imports = [
@@ -18,6 +18,19 @@
       haskell.haskell justusadam.language-haskell
       llvm-vs-code-extensions.vscode-clangd
       ms-python.python ms-pyright.pyright
+
+      (pkgs.vscode-utils.buildVscodeMarketplaceExtension rec {
+        mktplcRef = {
+          name = "lean4";
+          version = "0.0.183";
+          publisher = "leanprover";
+        };
+        vsix = builtins.fetchurl {
+          name = "${mktplcRef.publisher}-${mktplcRef.name}.zip";
+          url = "https://marketplace.visualstudio.com/_apis/public/gallery/publishers/leanprover/vsextensions/lean4/0.0.183/vspackage";
+          sha256 = "07mmjp7fh0v4dwqhb62v8khg5jns7ax79z5pdqw04aw16zv46jd8";
+        };
+      })
     ];
   };
 
@@ -37,5 +50,6 @@
     haskell-language-server cabal-install ghc
     bash
     python3 pyright poetry black
+    lean4 elan
   ];
 }
