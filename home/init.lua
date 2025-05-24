0bf384f270cca5b4a8d44862976f0617618010f6
Tali Auster <me@tali.network>
2025-05-23 19:26:15 -0600

    home: fixes and lua stuff
    commit messages don't matter it's all fine

--- home/init.lua
+++ home/init.lua
@@ -1,69 +1,76 @@
-  vim.cmd [[colorscheme catppuccin-latte]]
+vim.cmd [[colorscheme catppuccin-latte]]
 
-  local lspconfig = require('lspconfig')
-  lspconfig.clangd.setup{}
-  lspconfig.gopls.setup{}
-  lspconfig.hls.setup{}
-  lspconfig.svelte.setup{}
-  lspconfig.tinymist.setup{}
+local lspconfig = require('lspconfig')
+lspconfig.clangd.setup{}
+lspconfig.gopls.setup{}
+lspconfig.hls.setup{}
+lspconfig.svelte.setup{}
+lspconfig.tinymist.setup{}
 
-  lspconfig.nil_ls.setup {
-    settings = {
-      ['nil'] = {
-	nix = { flake = { autoArchive = true; }; };
-	formatting = { command = { "nixfmt" }; };
-      };
+lspconfig.nil_ls.setup {
+  settings = {
+    ['nil'] = {
+      nix = { flake = { autoArchive = true; }; };
+      formatting = { command = { "nixfmt" }; };
     };
-  }
+  };
+}
 
-  local set_spaces_callback = function (width)
-    return function()
-      vim.opt_local.shiftwidth = width;
-      vim.opt_local.tabstop = width;
-      vim.opt_local.expandtab = true;
-    end
+local set_spaces_callback = function (width)
+  return function()
+    vim.opt_local.shiftwidth = width;
+    vim.opt_local.tabstop = width;
+    vim.opt_local.expandtab = true;
   end
+end
 
-  vim.api.nvim_create_autocmd('FileType', {
-    pattern = 'nix';
-    callback = set_spaces_callback(2);
-  })
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'nix';
+  callback = set_spaces_callback(2);
+})
 
-  vim.api.nvim_create_autocmd('FileType', {
-    pattern = 'ada';
-    callback = set_spaces_callback(3);
-  })
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'lua';
+  callback = set_spaces_callback(2);
+})
 
-  vim.api.nvim_create_autocmd('LspAttach', {
-    callback = function(ev)
-      vim.opt.number = true
-      vim.api.nvim_create_autocmd("BufWritePre", {
-	callback = function(ev)
-	  vim.lsp.buf.format {bufnr = ev.buf}
-	end
-      })
-    end,
-  })
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'ada';
+  callback = set_spaces_callback(3);
+})
 
-  vim.diagnostic.config({
-    virtual_lines = true,
-  })
-
-  vim.g.mapleader = ' ';
-
-  local telescope = require('telescope.builtin');
-  vim.keymap.set('n', '<leader>a', telescope.live_grep, { desc = 'Live grep (telescope)'; });
-  vim.keymap.set('n', '<leader>o', telescope.find_files, { desc = 'Find files (telescope)'; });
-  vim.keymap.set('n', '<leader>e', telescope.buffers, { desc = 'Open buffer (telescope)'; });
-
-  vim.keymap.set('n', '<leader>ll', function()
-    vim.diagnostic.jump({
-      diagnostic = vim.diagnostic.get_next()
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(ev)
+    vim.opt.number = true
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      callback = function(ev)
+        vim.lsp.buf.format {bufnr = ev.buf}
+      end
     })
-  end, { desc = "Next diagnostic." })
+  end,
+})
 
-  vim.keymap.set('n', '<leader>lr', function()
-    vim.diagnostic.jump({
-      diagnostic = vim.diagnostic.get_next()
-    })
-  end, { desc = "Next diagnostic." })
+vim.diagnostic.config({
+  virtual_lines = true,
+})
+
+vim.g.mapleader = ' ';
+
+local telescope = require('telescope.builtin');
+vim.keymap.set('n', '<leader>a', telescope.live_grep, { desc = 'Live Grep (Telescope)'; });
+vim.keymap.set('n', '<leader>o', telescope.find_files, { desc = 'Find Files (Telescope)'; });
+vim.keymap.set('n', '<leader>e', telescope.buffers, { desc = 'Open Buffer (Telescope)'; });
+
+vim.keymap.set('n', '<leader>ll', function()
+  vim.diagnostic.jump({
+    diagnostic = vim.diagnostic.get_next()
+  })
+end, { desc = "Next diagnostic." })
+
+vim.keymap.set('n', '<leader>lr', function()
+  vim.diagnostic.jump({
+    diagnostic = vim.diagnostic.get_next()
+  })
+end, { desc = "Next diagnostic." })
+
+vim.keymap.set('n', '<leader>i', vim.lsp.buf.hover, { desc = 'LSP Hover'; });
